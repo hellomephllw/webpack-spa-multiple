3334112042aa66ac4fd4e856c840496a451ac136
llw <hellomephllw@163.com>
2017-02-16 13:16:29 +0800

    add vendor files

---
+++ src/common/scripts/stringUtils.js
@@ -0,0 +1,8 @@
+/**
+ * Created by wb-llw259548 on 2017/2/16.
+ */
+const stringUtils = {
+    init() {
+        console.log('stringUtils!!');
+    }
+};
---
+++ src/common/vendor.js
@@ -0,0 +1,5 @@
+/**
+ * Created by wb-llw259548 on 2017/2/16.
+ */
+import './css/reset.scss';
+import './scripts/stringUtils';
